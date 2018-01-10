bc17b507accc63d81cd4ef271fad6bec1466545c
caoxiaoyu <sioyu_job@163.com>
2018-01-10 15:15:21 +0800

    add pods

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform:ios, 8.0
+inhibit_all_warnings!
+
+target 'projectForJenkins' do
+    #facebook pop开源动画库
+    pod 'pop', '~> 1.0'
+end
+    
---
+++ Pods/Target Support Files/Pods-projectForJenkins/Pods-projectForJenkins-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_projectForJenkins : NSObject
+@end
+@implementation PodsDummy_Pods_projectForJenkins
+@end
